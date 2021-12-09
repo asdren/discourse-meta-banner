18b62c289a514bbb380c62f7f9eeab749c8efe97
asdren <asdren83@gmail.com>
2021-12-09 21:28:06 +0100

    Update meta-banner.js

--- javascripts/discourse/components/meta-banner.js
+++ javascripts/discourse/components/meta-banner.js
@@ -8,10 +8,9 @@ export default Component.extend({
   @discourseComputed("currentUser")
   shouldShow(currentUser) {
     const isStaff = currentUser && currentUser.staff;
-    const isStartpage =  url === "/";
     const lowTrustLevel = currentUser && currentUser.trust_level < 2;
     // show banner only for anons and < TL 2
-    return !isStaff && (!currentUser || lowTrustLevel) && isStartpage;
+    return !isStaff && (!currentUser || lowTrustLevel);
   },
 
   @discourseComputed("router.currentRouteName", "router.currentURL")
